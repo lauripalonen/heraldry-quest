2f834a7128ee0e64682e62368cab17dac45a4c1e
lauripalonen <palonen.lauri@gmail.com>
2025-07-02 21:18:20 +0300

    ensure new shuffle on each visit on quest-page

--- src/app/quest/page.tsx
+++ src/app/quest/page.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { QuestionContent } from '@/components/QuestionContent';
 import { getCoatOfArms } from '../actions';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Quest() {
     const coatOfArms = await getCoatOfArms()
 
