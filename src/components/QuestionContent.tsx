92042476a92908df2b99a935d4969d5eb7296f89
lauripalonen <palonen.lauri@gmail.com>
2025-07-02 20:57:08 +0300

    general improvements

--- src/components/QuestionContent.tsx
+++ src/components/QuestionContent.tsx
@@ -1,22 +1,19 @@
 "use client";
 
-import React from "react";
+import React, { Suspense, useEffect } from "react";
 import Image from "next/image";
 import { AnswerForm } from "./AnswerForm";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./ui/table";
 import { toast } from "sonner";
 import { CircleCheck, CircleX } from "lucide-react";
 import { Button, LinkButton } from "./ui/button";
-import { Card, CardContent } from "./ui/card";
+import { Card, CardContent, CardHeader } from "./ui/card";
 
 
 export function QuestionContent({ coatOfArms }: { coatOfArms: { image_url: string, armiger: string }[] }) {
-    const [index, setIndex] = React.useState(306);
+    const [index, setIndex] = React.useState(307);
     const [score, setScore] = React.useState({ correct: 0, incorrect: 0 });
-    const currentCoatOfArms = coatOfArms[index];
     const n = coatOfArms.length;
-
-    console.log("Current index:", index);
 
     const handleNext = () => {
         if (index < n) {
@@ -24,19 +21,22 @@ export function QuestionContent({ coatOfArms }: { coatOfArms: { image_url: strin
         }
     };
 
+    const currentCoatOfArms = coatOfArms[index];
+
     const handleScore = (isCorrect: boolean) => {
         if (isCorrect) {
             toast("Correct!", {
-                style: {
-                    width: "fit-content",
-                }, icon: <CircleCheck className="text-green-500" />
+                classNames: {
+                    toast: "!bg-emerald-100"
+                },
+                icon: <CircleCheck className="text-green-500" />
             });
             setScore((prevScore) => { return { ...prevScore, correct: prevScore.correct + 1 } });
         }
         else {
-            toast.error("Incorrect. The correct answer is: " + currentCoatOfArms.armiger, {
-                style: {
-                    width: "fit-content",
+            toast.error("Correct answer: " + currentCoatOfArms.armiger, {
+                classNames: {
+                    toast: "!bg-rose-100"
                 },
                 icon: <CircleX className="text-red-500" />
             });
@@ -55,19 +55,26 @@ export function QuestionContent({ coatOfArms }: { coatOfArms: { image_url: strin
 
     if (index === n) {
         return <div className="flex flex-col justify-center items-center">
-            <h3>Final score:</h3>
-            <ul>
-                <li>
-                    Questions: {n}
-                </li>
-                <li>
-                    Correct: {score.correct}
-                </li>
-                <li>
-                    Incorrect: {score.incorrect}
-                </li>
-            </ul>
-            <div>
+            <Card className="py-2 gap-0">
+                <CardHeader><h2>Final score</h2></CardHeader>
+                <CardContent className="px-2">
+                    <Table className="m-auto text-xs">
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Correct</TableHead>
+                                <TableHead>Incorrect</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell className="font-medium">{score.correct}</TableCell>
+                                <TableCell>{score.incorrect}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </CardContent>
+            </Card>
+            <div className="flex flex-row gap-4 items-baseline">
                 <Button onClick={resetQuest} className="mt-4">
                     Again
                 </Button>
@@ -77,10 +84,10 @@ export function QuestionContent({ coatOfArms }: { coatOfArms: { image_url: strin
     }
 
     return (
-        <div className="flex flex-col items-center justify-center gap-8">
-            <Card className="py-3">
-                <CardContent>
-                    <Table className="m-auto">
+        <div className="flex flex-col items-center justify-center gap-4">
+            <Card className="py-2">
+                <CardContent className="px-2">
+                    <Table className="m-auto text-xs">
                         <TableHeader>
                             <TableRow>
                                 <TableHead>Correct</TableHead>
@@ -98,8 +105,12 @@ export function QuestionContent({ coatOfArms }: { coatOfArms: { image_url: strin
                     </Table>
                 </CardContent>
             </Card>
-            <Image src={currentCoatOfArms.image_url} alt={`Coat of Arms ${index + 1}`} width={250} height={250} className="rounded-lg" />
-            <Card><CardContent><AnswerForm armiger={currentCoatOfArms.armiger} handleNext={handleNext} handleScore={handleScore} /></CardContent></Card>
+            <Image src={currentCoatOfArms.image_url} alt={`Coat of Arms ${index + 1}`} width={180} height={180} className="rounded-lg" />
+            <Card>
+                <CardContent>
+                    <AnswerForm armiger={currentCoatOfArms.armiger} handleNext={handleNext} handleScore={handleScore} />
+                </CardContent>
+            </Card>
         </div>
     )
 }   
