d090aa753243b6764e832bf993b2418d999959ba
lauripalonen <palonen.lauri@gmail.com>
2025-07-02 21:19:49 +0300

    increase font size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navigation() {
   return (
     <header className="p-2 flex items-baseline justify-center border-b-2 border-b-stone-800">
-      <Link className="font-title text-2xl md:text-6xl hover:text-amber-500 active:text-amber-500" href="/">
+      <Link className="font-title text-4xl md:text-6xl hover:text-amber-500 active:text-amber-500" href="/">
         Heraldry Quest
       </Link>
     </header>
